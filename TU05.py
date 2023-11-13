e7a3f643284516d015e9c81e2f9b878c1acaf3a2
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2023-11-13 10:17:18 +0800

    TU05.py

---
+++ TU05.py
@@ -0,0 +1,5 @@
+test = input("Is it raining? y/n: ")
+if test in ("y","Y","yes","YES"):
+  print ("Oh dear, no football today then.")
+if test in ("n","no","N","NO"):
+  print ("Great! We can play football today!")
