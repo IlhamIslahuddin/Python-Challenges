17ae07059b8ec08a1f66228fe689fc6fa00b31a4
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2023-11-28 13:40:41 +0800

    TU08.py

---
+++ TU08.py
@@ -0,0 +1,5 @@
+FavSubject = ""
+while FavSubject != "computer science":
+  FavSubject = str(input("What is the best subject? (only one right answer): "))
+  if FavSubject == "computer science":
+                   print ("correct")
