a2a8ab580dda1d645f947604b325710dc1aab335
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-01-29 13:32:10 +0800

    TU13.py

---
+++ TU13.py
@@ -0,0 +1,7 @@
+MyList = [9,4,8,11,2]
+
+print ("total:",sum(MyList))
+print ("minimum",min(MyList))
+print ("maximum:",max(MyList))
+print ("length:",len(MyList))
+print ("average:",(sum(MyList)/(len(MyList))))
