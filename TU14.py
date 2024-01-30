365c6014cbcc5b15e0d2896b15b4da1ec8c3b1c7
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-01-30 13:10:42 +0800

    TU14.py

---
+++ TU14.py
@@ -0,0 +1,7 @@
+mystring = "abcdefghijklmnopqrstuvwxyz"
+print (mystring)
+print (mystring[0:3]) #prints the first 3 letters
+print (mystring[3::]) # prints the whole string starting from the 4rd character
+print (mystring[0:6:2]) #the third number is the step between the characters
+print(mystring[-1:-7:-1]) #prints backwards and stops before last character
+print(mystring[0:-2])  #print until two from the last character
