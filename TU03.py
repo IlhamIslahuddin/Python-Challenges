3dfdf9a8f5a99750aa46c3c66214acfb1d78ffb0
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-03-25 13:01:41 +0800

    TU03.py

---
+++ TU03.py
@@ -0,0 +1,4 @@
+MyAge = int(input("What is your age? "))
+ComputerAge = (MyAge*7)
+print ("%d is a very nice age to be!" % MyAge)
+print ("Computer age is %.3f and i am older than %d so i am older than you." %(ComputerAge,MyAge))
