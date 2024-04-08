041cdfb286ea95b108db69aec9171ddd6f8c2c63
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-04-08 16:52:51 +0800

    TU23.py

---
+++ TU23.py
@@ -0,0 +1,11 @@
+count = 0
+my_byte = str(input("Input your byte: "))
+for i in my_byte:
+    if i == "1":
+      count = count +1
+print(count)
+
+if count%2 == 0:
+  print ("Even number of 1s")
+elif count%2 ==1:
+  print ("Odd number of 1s")
