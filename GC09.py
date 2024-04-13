837ac371d7492398c9cae369c9819b786db2b488
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-04-13 18:04:02 +0800

    GC09.py

---
+++ GC09.py
@@ -0,0 +1,5 @@
+for i in range (99,0,-1):
+    if i == 1:
+        print ("1 bottle of cola on the wall, 1 bottle of cola! Take one down pass it around, no more bottles of cola on the wall!")
+    else:
+        print(i,"bottles of cola on the wall,",i,"bottles of cola! Take one down, pass it around,",i-1,"bottles of coke on the wall.")
