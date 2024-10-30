e443559af0bf137a8d0fda971b7d41158b9107b9
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-10-30 14:18:31 +0800

    Update DadJokes OOP.py

--- DadJokes OOP.py
+++ DadJokes OOP.py
@@ -1,10 +1,16 @@
 import random
+
+jokes = []
 class Joke:
     def __init__(self):
         self.question = ""
         self.answer = ""
-        
+
 def read_jokes():
-    with open("DadJokes.txt","r") as f:
+    with open("DadJokes.txt", "r") as f:
         for line in f:
             readline = line
+            jokes.append(readline)
+    print (jokes)
+
+read_jokes()
